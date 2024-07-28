c8d738434c60980e7ef50f85ecacc71032a17b92
Yves Arthur <ashlynmerria@gmail.com>
2024-07-28 22:01:15 +0100

    Added Dynamic rendering with Suspense

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2ttLVWe9RvZqY5zZOFKAia4U0VYvG3GrD6+Ficd3uYc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
